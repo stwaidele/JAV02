367993d815828b783ca60beda4d2255c4cd223b0
Stefan Waidele <Stefan@Waidele.info>
2015-03-04 02:40:40 +0100

    Große Testläufe

---
+++ auswertung.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "Auswertung von " $1
+echo -n "Anzahl E: "
+cat $1 | grep -c "^E" 
+echo -n "Anzahl V: "
+cat $1 | grep -c "^V" 
+
+echo -n "E muss warten: "
+cat $1 | grep -c "^E.(...sek):   15 |" 
+echo -n "V muss warten: "
+cat $1 | grep -c "^V.(...sek):    0 |"
